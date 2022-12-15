2bfbbe3b672674da067419f112ddb19321feff2c
Harith AKareem <Harith.A.Kareem@Openbet.com>
2022-12-15 11:01:14 +0000

    rest template

---
+++ src/main/java/com/harith/springboot/client/SpringClient.java
@@ -0,0 +1,17 @@
+package com.harith.springboot.client;
+
+import com.harith.springboot.domain.Anime;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+public class SpringClient {
+    public static void main(String[] args) {
+        ResponseEntity<Anime> animeResponseEntity =  new RestTemplate().getForEntity("http://localhost:8080/animes/{id}", Anime.class, 1);
+        log.info("Response Entity {}", animeResponseEntity);
+        log.info("Response Data {}", animeResponseEntity.getBody());
+
+        Anime anime = new RestTemplate().getForObject("http://localhost:8080/animes/{id}", Anime.class, 1);
+    }
+}
